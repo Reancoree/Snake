51acc0b22e66d070f3d7b6a304c0923e71337d3e
герочка <reancoree@gmail.com>
2023-01-22 14:50:22 +0300

    Добавил логику в Input()

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -52,7 +52,27 @@ void Draw()
 }
 void Input()
 {
-
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'esc':
+			gameOver = false;
+			break;
+		}
+	}
 }
 void Logic()
 {
