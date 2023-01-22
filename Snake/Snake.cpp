c887c0e3291634463d4fc44b221ef1a1dbd2be00
герочка <reancoree@gmail.com>
2023-01-22 13:27:56 +0300

    Создал глобальные переменные и Setup()

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -2,9 +2,45 @@
 #include <conio.h>
 using namespace std;
 
-int main()
+bool gameOver;
+const int width = 30;
+const int height = 30;
+int x, y, fruitX, fruitY, score;
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+void Draw()
 {
 
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
 
 	return 0;
 }
