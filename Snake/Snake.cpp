b507128e00839124618cc094ec76c71bde0b3438
герочка <reancoree@gmail.com>
2023-01-23 00:01:09 +0300

    Добавил функциональное меню

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -9,8 +9,10 @@ const int height = 20;
 int x, y, fruitX, fruitY, score;
 enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 Direction dir;
-int tailX[100], tailY[100];
-int lTail;
+int tailX[1000], tailY[1000];
+int lTail; // length of tail
+int speed = 0; // speed of game
+bool throughWall = false; // gamemode
 
 void FixSnakeSpeed()
 {
@@ -122,7 +124,6 @@ void Logic()
 		tailY[i] = prevY;
 		prevX = prev2X;
 		prevY = prev2Y;
-
 	}
 
 	switch (dir)
@@ -143,6 +144,12 @@ void Logic()
 	if (x < 0 || x > width || y < 0 || y > height)
 		gameOver = true;
 
+	if (throughWall)
+	{
+		if (x >= width) x = 0; else if (x == 0) x = width - 1;
+		if (y >= height) y = 0; else if (y == 0) y = height - 1;
+	}
+
 	for (int i = 0; i < lTail; i++)
 		if (tailX[i] == x && tailY[i] == y)
 			gameOver = true;
@@ -153,13 +160,48 @@ void Logic()
 		score += 1;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+	}	
+}
+void StartScreen()
+{
+	bool start = false;
+
+	while (start != true)
+	{
+		system("cls");
+		cout << "\n\n\t 1. Choose speed\n\t 2. Choose Gamemode \n\t 3. Start game";
+		int choice; cout << "\n\t"; cin >> choice;
+	
+		switch (choice)
+		{
+		case 1:
+			cout << "1. Low\n2. Medium\n3. Fast\n";
+			int value; cin >> value;
+			if (value == 1)
+				speed = 200;
+			else if (value == 2)
+				speed = 75;
+			break;
+		case 3:
+			start = true;
+			break;
+		case 2:
+			cout << "Will the snake pass through the wall?\n\t 1. Yes\n\t2. No\n";
+			int gameModeValue; cin >> gameModeValue;
+			if (gameModeValue == 1)
+				throughWall = true;
+			else throughWall = false;
+			break;
+
+		default:			
+			break;
+		}
 	}
-		
 }
 
 int main()
 {
-	
+	StartScreen();
 	Setup();
 	while (!gameOver)
 	{
@@ -167,7 +209,7 @@ int main()
 		FixSnakeSpeed();
 		Input();
 		Logic();
+		Sleep(speed);
 	}
-
 	return 0;
 }
